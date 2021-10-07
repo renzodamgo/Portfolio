575f41a35306444a9a271888854046810be7d894
Renzo Damian <renzo.damian.go@gmail.com>
2021-10-07 18:27:12 -0500

    refactor with variables

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { CamComponent } from './projects/cam/cam.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",component:AppComponent,
+  },
+  {
+    path: "projects",children:[
+      {path:"camupc",component:CamComponent}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
