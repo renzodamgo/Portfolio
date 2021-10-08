4b873f1565c8b86534188430863b1620558ae7c0
Renzo Damian <renzo.damian.go@gmail.com>
2021-10-08 12:44:36 -0500

    added appear effect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,31 +3,60 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'Portfolio';
 
   ngOnInit(): void {
-    console.log("das")
-    let element = <HTMLInputElement> document.querySelector('input[name=theme]');
-    element?.addEventListener('change',()=>{
-      if (element.checked){
-        trans()
-        document.documentElement.setAttribute('data-theme','light')
-        
-      }else{
-        document.documentElement.setAttribute('data-theme','dark')
-
-      }
-    })
-
-    let trans = ()=>{
-      document.documentElement.classList.add('transition');
-      window.setTimeout(()=>{
-        document.documentElement.classList.remove('transition')
-      },1000)
-    }
+    // console.log("das")
+    this.changueTheme();
+    this.scrollObserver();
   }
 
+  changueTheme(): void {
+    let element = <HTMLInputElement>document.querySelector('input[name=theme]');
+    element?.addEventListener('change', () => {
+      if (element.checked) {
+        trans();
+        document.documentElement.setAttribute('data-theme', 'light');
+      } else {
+        document.documentElement.setAttribute('data-theme', 'dark');
+      }
+    });
+
+    let trans = () => {
+      document.documentElement.classList.add('transition');
+      window.setTimeout(() => {
+        document.documentElement.classList.remove('transition');
+      }, 1000);
+    };
+  }
+
+  scrollObserver(): void {
+    var faders = document.querySelectorAll('.fade-in');
+    console.log(faders);
+
+    const appearOptions = {
+      threshold: 1,
+      rootMargin: "0px 0px -50px 0px"
+    };
+
+    const appearOnScroll = new IntersectionObserver(
+      (entries, appearOnScroll) => {
+        entries.forEach((entry) => {
+          if (!entry.isIntersecting) {
+            return;
+          } else {
+            entry.target.classList.add('appear');
+            appearOnScroll.unobserve(entry.target);
+          }
+        });
+      },
+      appearOptions
+    );
+    faders.forEach(fader=>{
+      appearOnScroll.observe(fader);
+    })
+  }
 }
